801cbb74e21f9d303b982acf659545ad9fe1d6d9
Tychang022 <156855881+Tychang022@users.noreply.github.com>
2024-03-08 15:50:29 -0800

    main_write.cpp

--- main_write.cpp
+++ main_write.cpp
@@ -4,41 +4,43 @@
 using namespace std;
 
 int main()
-{	
+{
 	ofstream ofs;
-	ofs. open("student.txt");
-	if (!ofs) {
+	ofs.open("students.txt");
+	if (!ofs)
+	{
 		cout << "Open Error\n";
 		exit(0);
 	}
-	int N; 
-	int M = 2; 
+	int N;
+	int M = 2;
 	double average, score, sum = 0;
 	string studentName;
-do{
-	ofs << "Enter the number of students: " ;
-	cin >> N;
-	if (N < 0)
-		cout << "Invalid Input. Please try again\n ";		
-} while (N < 0);
-	
-	for(int i=0; i<N; i++)
+	do
+	{
+		cout << "Enter the number of students: ";
+		cin >> N;
+		if (N < 0)
+			cout << "Invalid Input. Please try again\n ";
+	} while (N < 0);
+
+	for (int i = 0; i < N; i++)
 	{
 		sum = 0;
 		cout << "What is the name of the student \n";
 		cin >> studentName;
 		ofs << studentName << " ";
-		for (int j=0; j < M; j++)
+		for (int j = 0; j < M; j++)
 		{
 			cout << "What is the score for " << studentName << endl;
 			cin >> score;
+			ofs << score << " ";
 			sum += score;
 		}
 		average = sum / M;
 		ofs << sum << " " << average << endl;
-	
 	}
-	ofs.close(); 
-    	cout << "Data has been written to student.txt." << endl;
-    	return 0;
+	ofs.close();
+	cout << "Data has been written to student.txt." << endl;
+	return 0;
 }
