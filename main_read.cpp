597006bab2b03bc530163e1fa6c12b103392c253
Tychang022 <156855881+Tychang022@users.noreply.github.com>
2024-03-08 14:04:49 -0800

    main_read.cpp

--- main_read.cpp
+++ main_read.cpp
@@ -1,43 +1,31 @@
 #include <iostream>
 #include <fstream>
-#include <string>
+
 using namespace std;
 
-int main()
-{	
-	ofstream ofs;
-	ofs. open("student.txt");
-	if (!ofs) {
-		cout << "Open Error\n";
-		exit(0);
-	}
-	int N; 
-	int M = 2; 
-	double average, score, sum = 0;
-	string studentName;
-do{
-	cout << "Enter the number of students: " ;
-	cin >> N;
-	if (N < 0)
-		cout << "Invalid Input. Please try again\n ";		
-} while (N < 0);
-	
-	for(int i=0; i<N; i++)
-	{
-		sum = 0;
-		cout << "What is the name of the student \n";
-		cin >> studentName;
-		for (int j=0; j < M; j++)
-		{
-			cout << "What is the score for " << studentName << endl;
-			cin >> score;
-			sum += score;
-		}
-		average = sum / M;
-		cout << studentName << "sum: " << sum << " average: " << average << endl;
-	
-	}
-	ofs.close(); 
-    	cout << "Data has been written to student.txt." << endl;
-    	return 0;
+int main() {
+    ifstream inputFile("student.txt");
+    if (!inputFile.is_open()) {
+        cout << "Error opening file." << endl;
+        exit (0);
+    }
+
+    string studentName;
+    int score1, score2, sum;
+    double average;
+
+    while (inputFile >> studentName >> score1 >> score2 >> sum >> average) {
+        
+        average = sum / 2.0;
+
+        cout << "Student: " << studentName << endl;
+        cout << "Score 1: " << score1 << endl;
+        cout << "Score 2: " << score2 << endl;
+        cout << "Sum: " << sum << endl;
+        cout << "Average: " << average << endl;
+        cout << endl;
+    }
+
+    inputFile.close();
+    return 0;
 }
