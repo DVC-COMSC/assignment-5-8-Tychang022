1590f0f66727c0f79255303d7003cb7cc46b930e
Tychang022 <156855881+Tychang022@users.noreply.github.com>
2024-03-08 11:10:20 -0800

    main_read.cpp

--- main_read.cpp
+++ main_read.cpp
@@ -6,36 +6,34 @@ using namespace std;
 int main()
 {	
 	ofstream ofs;
-	ofs. open("student.txt")
+	ofs. open("student.txt");
 	if (!ofs) {
 		cout << "Open Error\n";
 		exit(0);
 	}
 	int N; 
-	int M; 
+	int M = 2; 
 	double average, score, sum = 0;
 	string studentName;
 do{
 	cout << "Enter the number of students: " ;
 	cin >> N;
-	cout << "Enter the number of scores for each student: " ;
-	cin >> M;
-	if (N < 0 || M < 0)
+	if (N < 0)
 		cout << "Invalid Input. Please try again\n ";		
-} while (N < 0 || M < 0);
+} while (N < 0);
 	
 	for(int i=0; i<N; i++)
 	{
 		sum = 0;
-		cout << "What is the name of the student ";
+		cout << "What is the name of the student \n";
 		cin >> studentName;
-		for (int j=0; j<M; j++)
+		for (int j=0; j < M; j++)
 		{
 			cout << "What is the score for " << studentName << endl;
 			cin >> score;
 			sum += score;
 		}
-		average = sum / N * M;
+		average = sum / M;
 		cout << studentName << "sum: " << sum << " average: " << average << endl;
 	
 	}
